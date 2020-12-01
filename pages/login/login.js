2900871c2a40aa019ea7f1a7d7fd1674ad7c8062
liangan <1449368524@qq.com>
2020-12-01 10:41:58 +0800

    wx请求

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,7 @@
 // pages/login/login.js
 Page({
   data: {
+    code: 'USD',
     login:'登录',
     pwd:'密码',
     array: [1, 2, 3, 4, 5],
@@ -29,5 +30,21 @@ Page({
     var userPwd = data.detail.value.password;
     console.log("userName---> " + userName);
     console.log("userPwd---> " + userPwd);
+    var that = this;
+    wx.request({
+      url: 'https://myserver.com:8080', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    })
+
+    
   }
 })
